740f85ec58ca40bf12645ba77aa6cd7e5df01995
“chandan” <chandanabbupur@gmail.com>
2024-10-25 11:58:21 +0530

    d

--- 05-03-Utility-Project/terraform-manifests/c1-versions.tf
+++ 05-03-Utility-Project/terraform-manifests/c1-versions.tf
@@ -7,22 +7,11 @@ terraform {
       version = ">= 5.0"
     }
   }
-  # Adding Backend as S3 for Remote State Storage
-  backend "s3" {
-    bucket = "terraform-on-aws-for-ec2cc"
-    key    = "dev/project1-vpc/terraform.tfstate"
-    region = "ap-southeast-2" 
-
-    # Enable during Step-09     
-    # For State Locking
-    dynamodb_table = "dev-project1-vpc"    
-  }    
 }
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
-  profile = "default"
+  region  = "us-east-1"
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
