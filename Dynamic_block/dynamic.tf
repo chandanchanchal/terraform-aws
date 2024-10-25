627b76fd30f7923e0d92102bd735a9a1b3680472
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-10-25 14:30:02 +0530

    Create dynamic.tf

---
+++ Dynamic_block/dynamic.tf
@@ -0,0 +1,123 @@
+# Provider configuration for AWS
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Variable to define ingress rules as a list of maps
+variable "ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description = "Allow SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+# Security Group resource with dynamic ingress rules
+resource "aws_security_group" "my_sg" {
+  name        = "example-security-group"
+  description = "Security group with dynamic ingress rules"
+
+  # Dynamic block for ingress rules
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  # Example egress rule allowing all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Output the security group ID
+output "security_group_id" {
+  description = "ID of the created security group"
+  value       = aws_security_group.my_sg.id
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules for the security group"
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [
+    {
+      description = "Allow SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      description = "Allow HTTP"
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "example-security-group"
+  description = "Security group with dynamic ingress rules"
+
+  dynamic "ingress" {
+    for_each = var.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # -1 means all protocols
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "security_group_id" {
+  description = "ID of the created security group"
+  value       = aws_security_group.my_sg.id
+}
